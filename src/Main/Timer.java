87913a48e9ac8ecfac28785391c4d4d9e59392d0
Limonchegggg <80949663+Limonchegggg@users.noreply.github.com>
2022-03-30 06:22:30 +0300

    Правки

--- src/Main/Timer.java
+++ src/Main/Timer.java
@@ -4,11 +4,15 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import net.md_5.bungee.api.ChatColor;
+
 public class Timer {
 	private int time = 0;
 	private Messages msg = new Messages();
 	private ConfigGetter cg = new ConfigGetter();
 	private Main main = Main.getPlugin(Main.class);
+	private int finalTime = cg.RestartTime();
+	private int a = 10;
 	public Timer() {
 		new BukkitRunnable() {
 			
@@ -16,23 +20,26 @@ public class Timer {
 			@Override
 			public void run() {
 				time++;
-				if(time == cg.leftTimeMessage()) {
-					if(Bukkit.getOnlinePlayers() != null) {
-						for(Player player : Bukkit.getOnlinePlayers()) {
-							msg.sendMessage(player);
-						}
-					}
-				}
-				if(time == (cg.RestartTime()-5)) {
+				if(time == (finalTime-30)) {
 					for(Player player : Bukkit.getOnlinePlayers()) {
-						player.sendTitle("Рестарт сервера!", "5 секунд");
+						
+						player.sendTitle(ChatColor.DARK_GREEN + "Рестарт сервера!",ChatColor.GOLD + "через 30 секунд");
 					}
 				}
-				if(time >= cg.RestartTime()) {
-					time = 0;
+				if(time == (finalTime-a)) {
+					for(Player player : Bukkit.getOnlinePlayers()) {
+						player.sendTitle(ChatColor.YELLOW + "До рестарта",""+a);
+					}
+					a--;
+				}
+				if(time >= finalTime) {
+					time=0;
+					a=10;
 					msg.kickAll();
 					main.getServer().dispatchCommand(main.getServer().getConsoleSender(), "save-all");
+					if(Bukkit.getOnlinePlayers().size() == 0) {
 					Bukkit.spigot().restart();
+					}
 				}
 			}
 		}.runTaskTimer(main, 20L, 20L);
